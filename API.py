507ec41ecceac6e734efaa79233f124c78e187eb
philippebertrand22 <90271955+philippebertrand22@users.noreply.github.com>
2025-05-28 17:52:34 -0400

    API fetches historical weather data for a few days ago

---
+++ API.py
@@ -0,0 +1,34 @@
+#%%
+import urllib.request
+import sys
+
+import csv
+import codecs
+
+from datetime import datetime, timedelta
+
+yesterday = datetime.today() - timedelta(days=1)
+few_days_ago = yesterday - timedelta(days=3)
+yesterday = yesterday.strftime("%Y-%m-%d")
+few_days_ago = few_days_ago.strftime("%Y-%m-%d")
+        
+try: 
+  ResultBytes = urllib.request.urlopen(f"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Montreal/{few_days_ago}/{yesterday}?unitGroup=us&include=hours&key=KXQ7UNVYVT72UREE3L7WHCX8K&contentType=csv")
+  
+  # Parse the results as CSV
+  csv_text = csv.reader(codecs.iterdecode(ResultBytes, 'utf-8'))
+  
+  with open('WeatherData.csv', 'w', newline='') as csvfile:
+    csv_writer = csv.writer(csvfile)
+    for row in csv_text:
+      csv_writer.writerow(row)
+        
+except urllib.error.HTTPError  as e:
+  ErrorInfo= e.read().decode() 
+  print('Error code: ', e.code, ErrorInfo)
+  sys.exit()
+except  urllib.error.URLError as e:
+  ErrorInfo= e.read().decode() 
+  print('Error code: ', e.code,ErrorInfo)
+  sys.exit()
+#%%  
